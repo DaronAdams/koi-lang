039c75369aedfc911bc49a1af62c72cbe539e25f
DaronAdams <daron.cole@icloud.com>
2023-05-08 13:06:18 -0500

    Created base statement class

--- src/interpreter/Expr.java
+++ src/interpreter/Expr.java
@@ -2,14 +2,14 @@ package interpreter;
 
 import java.util.List;
 
-public abstract class Expr {
+abstract class Expr {
   interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
     R visitUnaryExpr(Unary expr);
   }
- public static class Binary extends Expr {
+  static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
@@ -37,7 +37,7 @@ public abstract class Expr {
 
     final Expr expression;
   }
-  public static class Literal extends Expr {
+  static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
     }
@@ -49,7 +49,7 @@ public abstract class Expr {
 
     final Object value;
   }
-  public static class Unary extends Expr {
+  static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
